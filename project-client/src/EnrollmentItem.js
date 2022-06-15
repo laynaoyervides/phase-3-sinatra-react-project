3df8a409af2758e65d9a1f56c5178ba47d17a5a5
Layna Oyervides <laynaoyervides@gmail.com>
2022-06-15 10:41:17 -0600

    Creating EnrollmentItem and EnrollmentList modules

---
+++ project-client/src/EnrollmentItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EnrollmentItem ({enrollment}) {
+    return (
+        <div className="enrollment_item_container">
+
+        </div>
+    )
+}
+
+export default EnrollmentItem;
