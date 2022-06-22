5e00911252b3ceb4f7ceacf044e98783f8603020
Layna Oyervides <laynaoyervides@gmail.com>
2022-06-22 09:16:21 -0600

    Adding instructor delete button

--- project-client/src/InstructorDetail.js
+++ project-client/src/InstructorDetail.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-function InstructorDetail ({instructorName}) {
+function InstructorDetail ({instructorName, onDeleteInstructor,}) {
+    function handleDeleteClick() {
+        fetch(`http://localhost:9292/instructors/${instructor.id}`, {
+            method: "DELETE",
+          })
+            .then((r) => r.json())
+            .then((deletedInstructor) => onDeleteInstructor(deletedInstructor));
+    }
  
     return (
             <div>
                 <h3>{instructorName}</h3>
                 <h5>EDIT</h5>
-                <h5>DELETE</h5>
+               <button onClick={handleDeleteClick}><h5>DELETE</h5></button>
             </div>
             
     );
