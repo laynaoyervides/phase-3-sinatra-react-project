5ae23bd8202eb1b082c03dfe786409f251db4a1e
Layna Oyervides <laynaoyervides@gmail.com>
2022-07-07 19:06:01 -0600

    Adding CRUD functions for Courses on Enrollments

--- project-client/src/CourseEnrollment.js
+++ project-client/src/CourseEnrollment.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React {useState, useEffect}from "react";
+import NewCourse from "./NewCourse";
+import { useEffect, useState } from "react";
 
 function CourseEnrollment () {
+    const [courses, setCourses] = useState([])
+    //get list of courses
+    useEffect ( ()  => {
+        fetch("http://localhost:9292/courses)
+        .then ((r) => r.json())
+        .then ((courses) => setCourses(courses));
+    }, []);
+
+     // Add a new course - CREATE - 
+     const addNewCourse= (course) => {
+        setCourses([...courses, courses]);
+    }
+    //handle the edit - UPDATE -
+    function handleUpdateCourse(updatedCourse){
+        const updatedCourses = courses.map((course) => {
+            if (course.id === updatedCourse.id) {
+                return updatedCourse;
+            }
+            else{
+                return course;
+            }
+        });
+        setCourses(updatedCourses);
+    }
+    // delete a course - DELETE -
+    function deleteCourse (id) {
+        const updatedCourses = courses.filter((course) => 
+            course.id !== id);
+        setCourses(updatedCourses);
+}
     return(
         <div>
-
+            <NewCourse />
         </div>
     )
 }
