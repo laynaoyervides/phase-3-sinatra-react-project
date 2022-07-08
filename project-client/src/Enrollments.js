d38d571caffaa05f4dd09f7ad4ee485363c89480
Layna Oyervides <laynaoyervides@gmail.com>
2022-07-07 18:57:37 -0600

    Adding NewCourse module

--- project-client/src/Enrollments.js
+++ project-client/src/Enrollments.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Enrollments.modules.css"
 import InstructorsList from "./InstructorsList";
-
+import CourseEnrollment from "./CourseEnrollment";
 
 function Enrollments() {
     return (
@@ -19,7 +19,7 @@ function Enrollments() {
                 </div>
                 <div className={styles.delete}>
                      <h3>VIEW, ADD, OR DELETE A COURSE </h3>
-                     
+                        <CourseEnrollment />
                      <p>Find the student and delete their enrollment(...Coming Soon...)</p>
                 </div>
             </div>
---
+++ project-client/src/NewCourse.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NewCourse () {
+    return(
+        <div></div>
+    )
+}
+
+export default NewCourse;
