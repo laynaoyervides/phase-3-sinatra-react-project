98a65b5260b0d2c6551a05dd1e67922d5bc8047a
Layna Oyervides <laynaoyervides@gmail.com>
2022-07-09 14:34:41 -0600

    Adding POST and object configuration for NewCourse module

--- project-client/src/NewCourse.js
+++ project-client/src/NewCourse.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 
-function NewCourse () {
+function NewCourse ({addNewCourse}) {
     const [courseName, setCourseName] = useState("")
     const [classPeriod, setClassPeriod] = useState("")
     const [instructor, setInstructor] = useState("");
@@ -11,6 +11,23 @@ function NewCourse () {
         class_period,
         instructor_id: instructor,
         
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCourse),
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(`http://localhost:9292/courses`, configObj)
+        .then ((resp) => resp.json())
+        .then ((course) => addNewCourse(course))
     }
     return(
         <div></div>
